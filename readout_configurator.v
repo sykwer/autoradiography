8c1ce36bd38800bd72591a2cce41db65a01f83b6
sykwer <sykwer@gmail.com>
2020-01-25 18:07:49 +0900

    Add readout configurator

---
+++ readout_configurator.v
@@ -0,0 +1,14 @@
+module readout_configurator(
+  SMT_MD, PDZ, NAPZ, ENTRI, INTUPZ, PGA,
+);
+  output SMT_MD, PDZ, NAPZ, ENTRI, INTUPZ;
+  output [2:0] PGA;
+
+  assign SMT_MD = 0; // Sequential mode
+  assign PDZ = 1; // active low
+  assign NAPZ = 1; // active low
+  // TODO: Add explanation
+  assign ENTRI = 0; // ???
+  assign INTUPZ = 0; // ???
+  assign PGA = 3; // ???
+endmodule
